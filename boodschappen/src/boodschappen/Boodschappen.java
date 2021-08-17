bd6ab44d9dee7a885a44cbfc3d85598e118d0cca
Gerard Doets <ahtec@users.noreply.github.com>
2021-08-17 17:01:35 +0200

    test alles

---
+++ boodschappen/src/boodschappen/Boodschappen.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boodschappen;
+
+
+
+
+/**
+ *
+ * @author User
+ */
+public class Boodschappen   {
+
+    /**
+     * @param args the command line arguments
+     */
+        public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+//                new NewFrame().setVisible(true);
+            }
+        });
+    }
+    
+//    
+//    public static void main(String[] args) {
+//        NewFrame t = new NewFrame();
+//        // TODO code application logic here
+//    }
+//    
+}
